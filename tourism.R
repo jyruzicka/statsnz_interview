d166c0a149df3357b007df4cfb0fff9ff5a31d87
Jan-Yves Ruzicka <jan@1klb.com>
2017-02-12 18:12:59 +1300

    Updated tourism plot for black-and-white

--- tourism.R
+++ tourism.R
@@ -1,8 +1,7 @@
 library("tidyverse")
-
+library("readxl")
 months <- c("Jan", "Feb", "Mar", "Apr", "May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
-
-# Data source: MBIE
+# Data source: MBIE monthly regional tourism estimates
 tourism <- read_csv("tourism.csv") %>%
   rename(Region = REGION) %>%
   filter(Region == "Canterbury") %>%
@@ -11,18 +10,19 @@ tourism <- read_csv("tourism.csv") %>%
     Year = format(Date,"%Y"),
     Month= factor(format(Date,"%b"),levels=months) 
   ) %>%
-  filter(
-    Year > 2013 & (
-      Month == "Jul" | Month == "Aug" |
-      Month == "Sep" | Month == "Oct" |
-      Month == "Nov" | Month == "Dec"
-    )
-  ) %>%
+  filter(Year > 2013 & (Month == "Jul" | Month == "Aug" | Month == "Sep" | Month == "Oct" |
+                          Month == "Nov" | Month == "Dec")) %>%
   group_by(Month,Year) %>%
   summarise(Total.Spend = sum(Spend))
+
+tourism.labels <- tourism[tourism$Month == "Sep", ]
 
 ggplot(data=tourism) +
   geom_line(mapping=aes(Month,Total.Spend,group=Year,colour=Year)) +
   xlab("Month") +
   ylab("Total monthly spending [million NZD]") +
-  labs(title="November 2016 tourism spending for Canterbury slumps")
+  labs(title="November 2016 tourism spending for Canterbury slumps") +
+  annotate("text", x="Sep", y=162, label="2016") +
+  annotate("text", x="Sep", y=152, label="2015") +
+  annotate("text", x="Sep", y=141, label="2014") +
+  theme(legend.position = "none")
